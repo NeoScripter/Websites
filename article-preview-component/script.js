04e581ded5ff90c8cd6cd3664e3959285334e54a
Ilya <sange0337@gmail.com>
2024-03-02 20:16:10 +0800

    Fixed disappearing of social links when not keeping the cursor at the share link

--- article-preview-component/script.js
+++ article-preview-component/script.js
@@ -4,19 +4,23 @@ document.addEventListener('DOMContentLoaded', function() {
     const svgPath = document.querySelector('.svg-icon path');
     let timeoutId;
 
-    shareBtn.addEventListener('mouseover', function() {
-        clearTimeout(timeoutId);
+    function showSocialLinks() {
+        clearTimeout(timeoutId); 
         socialLinks.style.display = 'flex';
         svgPath.style.fill = 'white';
         shareBtn.style.backgroundColor = 'hsl(217, 19%, 55%)';
-    });
-    
+    }
 
-    shareBtn.addEventListener('mouseout', function() {
+    function hideSocialLinks() {
         timeoutId = setTimeout(function() {
             socialLinks.style.display = 'none';
             shareBtn.style.backgroundColor = 'hsla(212, 23%, 69%, 0.2)';
             svgPath.style.fill = '';
         }, 500);
-    });
+    }
+
+    shareBtn.addEventListener('mouseover', showSocialLinks);
+    shareBtn.addEventListener('mouseout', hideSocialLinks);
+    socialLinks.addEventListener('mouseover', showSocialLinks);
+    socialLinks.addEventListener('mouseout', hideSocialLinks);
 });
